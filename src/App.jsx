830f133bfe1b2ff7117adfc06ecde2201667bd71
RaktimYoddha <raktimyoddha012@gmail.com>
2023-04-29 21:41:20 +0530

    footer import problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes,  Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 import AboutPage from "./pages/AboutPage";
 import ProjectPage from "./pages/ProjectPage";
 import ContactPage from "./pages/ContactPage";
